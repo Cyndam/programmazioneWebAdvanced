7e9c6cd40141c21726a90b85d3c1378c5e0a3c64
Gianlugi <gianluigificco79@gmail.com>
2024-04-10 22:58:38 +0200

    aggironamento progetto

--- esercitazione/eser.js
+++ esercitazione/eser.js
@@ -1,27 +1,35 @@
-addEventListener('DOMContentLoaded', (evt) =>{
-const btn = document.querySelector('.btn');
+document.addEventListener('DOMContentLoaded', (evt) =>{
+    const btn = document.querySelector('.btn');
+    const div = document.querySelector('.div-vuoto');
+    const lisOrd = document.querySelector('.lista-pres');
+    const btn2 = document.querySelector('.btn2');
 
-btn.addEventListener('click', function() {
-    const div = document.querySelector('.div-vuoto')
-    const newP = document.createElement('p');
-    const text = document.createTextNode('i am here')
-    newP.appendChild(text);
-    div.appendChild(newP);
+    btn.addEventListener('click', function() {
+        const newP = document.createElement('p');
+        const text = document.createTextNode('i am here');
+        newP.appendChild(text);
+        div.appendChild(newP);
+        // Cambia lo sfondo della lista non ordinata quando viene cliccato il pulsante btn
+        lisOrd.style.backgroundColor = 'red';
+    });
 
-    let par = document.getElementById("par")
-    lisOrd.style.backgroundColor = 'red';
+    btn2.addEventListener('click', () => {
+        let inserimento1 = parseInt(document.querySelector('#inserimento1').value);
+        let inserimento2 = parseInt(document.querySelector('#inserimento2').value);
 
-})
-    let lisOrd = document.querySelector('.lista-pres')
-    let newEl = document.createElement('li');
-    let newLis = document.createElement('ol');
-    let newElOl = document.createElement('li');
+        if(!isNaN(inserimento1) && !isNaN(inserimento2)) {
+            alert(inserimento1 + ' + ' + inserimento2 + ' = ' + somma(inserimento1, inserimento2));
+            // Aggiunge un nuovo elemento <li> alla lista ordinata quando viene cliccato il pulsante btn2
+            const newElOl = document.createElement('li');
+            newElOl.textContent = 'i am here';
+            lisOrd.appendChild(newElOl);
+        } else {
+            alert('Inserisci due numeri validi');
+        }
+    });
 
-
-    newElOl.textContent = 'i am here';
-    newLis.appendChild(newElOl);
-    newEl.appendChild(newLis);
-    lisOrd.appendChild(newEl);
-
-
-})
+    function somma(a, b) {
+        console.log(a + b);
+        return a + b;
+    }
+});
